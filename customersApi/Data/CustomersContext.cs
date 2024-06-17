f3644e232f0f508c463042d9201dd447f246e471
devote boniface <107687435+Boniface-devote@users.noreply.github.com>
2024-06-17 10:53:55 +0300

    added file

---
+++ customersApi/Data/CustomersContext.cs
@@ -0,0 +1,13 @@
+﻿using customersApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace customersApi.Data
+{
+    public class CustomersContext : DbContext
+    {
+        public CustomersContext(DbContextOptions<CustomersContext> options) : base(options) { }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ customersApi/Program.cs
@@ -0,0 +1,33 @@
+using customersApi.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddDbContext<CustomersContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder => builder.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader());
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.UseCors("AllowAll");
+app.MapControllers();
+app.Run();
